0bb4bc3635b2e90206d8881d24e7ead1502bea0a
VoskanianGor <vosckaniangor@gmail.com>
2022-12-26 20:31:13 +0300

    feat:  init setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>Wonderful things</title>
+				<meta
+					name="description"
+					content="Shop our wide selection of high-quality outdoor gear and equipment for all your adventures. From tents and sleeping bags to backpacks and hiking boots, we have everything you need for your next outdoor adventure. Find great deals on top brands and free shipping on all orders."
+				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<section>
+        {/* TODO/ add future page! */}
+      </section>
+		</>
+	)
+}
